ea25db220a61466cfb30931ec59979f6fb2bb407
aacczury <yl931905@gmail.com>
2014-03-05 21:36:44 +0800

    ipaddress

--- server.js
+++ server.js
@@ -35,6 +35,6 @@ app.get('/', function(req, res) {
 });
 //app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port'), app.get('ipaddress'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
