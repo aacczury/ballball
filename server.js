ffea4e8ae18bd815680e4cfa3a6344c0fae31ddb
aacczury <yl931905@gmail.com>
2014-03-05 19:58:06 +0800

    modify root

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var app = express();
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -28,8 +28,11 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/', function(req, res) {
+	var comments = [];
+	res.render('story', { comments: comments });
+});
+//app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
