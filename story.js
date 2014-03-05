4b63348826e608f37b291609a4716d7ce67bbc55
aacczury <yl931905@gmail.com>
2014-03-05 21:51:48 +0800

    add story

---
+++ story.js
@@ -0,0 +1,18 @@
+var db = require('mongoskin').db("mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/story");
+
+exports.findAll = function(req, res) {
+	//db.collection('comments').find().toArray(function(err, comments) {
+	var comments = [];
+		console.log(comments);
+		res.render('story', { comments: comments });
+	//});
+}
+
+exports.addComment = function(req, res) {
+	var comment = req.body;
+	console.log('Adding comment: ' + JSON.stringify(comment));
+	db.collection('comments').insert(comment, function(err, result){
+		console.log(result);
+		res.redirect('/');
+	});
+}
